9327196009c462e7d7c29793d09297abe6f635f9
lauramarti00 <879707@stud.unive.it>
2023-08-04 12:25:41 +0200

    creation of a parametric route for the execution of the query

--- app/routes.py
+++ app/routes.py
@@ -28,14 +28,25 @@ def index():
  
     form = SearchForm()
     if form.validate_on_submit():
+        film = form.title.data
+        print(film)
+        return redirect("/index/"+film)
         #flash('Film searched with title {}'.format(form.title.data))
         #return redirect(url_for('film_page'))
-        film = getFilmByTitle(form.title.data)
-        return render_template('index.html', title='Home', user=user, film=film, form=form)
-
+        #film = getFilmByTitle(form.title.data)
+        #return render_template('index.html', title='Home', user=user, film=film, form=form)
     return render_template('index.html', title='Home', user=user, form=form)
 
-
-@app.route('/film_page')
-def film_page():
-    return 'this is film page'
+#CAMBIARE INDIRIZZO PER TSUNG
+@app.route('/index/<_title>')
+def film_page(_title):
+    res = getFilmByTitle(_title)
+    print(res)
+    string = ""
+    if (res):
+        string = str(res)
+    else:
+        string = "no film found"    
+    return string
+   # return 'this is film page:  ' + _title + " QUERY RESULT:  " + string
+   
